ebfa1fb42e180fd7e5497fe0a08d4722badd2523
KevinYu <kevin@KevinYudeMacBook-Pro.local>
2018-07-11 16:41:50 +0800

    redux-counter

---
+++ src/redux-counter/js/components/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import Counter from '../containers/Counter'
+import Operator from '../containers/Operator'
+
+const App = () => {
+    return (
+        <div>
+            <Counter />
+            <Operator />
+        </div>
+    )
+}
+
+export default App;
---
+++ src/redux-counter/js/components/Counter.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const Counter = ({ num }) => {
+    return (
+        <p>{num}</p>
+    )
+}
+
+export default Counter
